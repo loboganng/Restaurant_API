7ef9149c4e5e40f83daee978dbe64bc04fd75999
Wolfgang Neves <wolfgang.neves@gmail.com>
2026-01-16 14:53:49 -0300

    In this commit I've created a personalized class (AppError) to handle error, and also a middleware to treat these errors

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from "express"
+import { AppError } from "@/utils/AppError.js";
 
 
 //Creating controller class to manage use cases
@@ -6,7 +7,7 @@ class ProductController {
   //Function to add index to class
   async index(request: Request, response: Response, next: NextFunction){
     try {
-      
+
       return response.json({ message: "ok"})
     } catch (error) {
       next(error);
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 //Importing dependencies
 import express from "express"
 import { routes } from "./routes/index.js"
+import { errorHandling } from "./middlewares/error-handling.js"
 
 //Creating const for the port and express
 const PORT = 3333
@@ -9,6 +10,8 @@ const app = express()
 //Using routes
 app.use(express.json())
 app.use(routes)
+
+app.use(errorHandling)
 
 //Listening to server on PORT (3333)
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
