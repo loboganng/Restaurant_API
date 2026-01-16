172e2d94b1c7b42842abcd31d6986f848c2e4673
Wolfgang Neves <wolfgang.neves@gmail.com>
2026-01-16 14:07:18 -0300

    Initial creation of "product-routes.ts"  with the imports and initial "get" metho, and also created the initial products-controller.ts, which will handle all the use cases.

---
+++ src/controllers/products-controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express"
+
+class ProductController {
+  async index(request: Request, response: Response, next: NextFunction){
+    try {
+      
+      return response.json({ message: "ok"})
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+export { ProductController }
---
+++ src/routes/products-routes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express"
+
+const productsRoutes = Router()
+
+productsRoutes.get("/", () => {})
+
+export { productsRoutes }
