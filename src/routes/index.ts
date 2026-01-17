dd95c6aa2c2d5a765bdf931f00be8d74d79bf33a
Wolfgang Neves <wolfgang.neves@gmail.com>
2026-01-17 13:23:11 -0300

    feat: Creating "tables" table in the database and populating it. Also creating INDEX route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 
 import { productsRoutes } from "./products-routes.js";
+import { tablesRoutes } from "./tables-routes.js";
 
 //Listing the index of all the routes
 
 const routes = Router()
 routes.use("/products", productsRoutes)
+routes.use("/tables", tablesRoutes)
 
 export { routes }
